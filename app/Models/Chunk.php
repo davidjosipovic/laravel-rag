1f55e866e888680334078ff921b4d8a064b5146d
Ivan Josipović <148684758+IIvell@users.noreply.github.com>
2026-08-02 13:52:12 +0200

    first commit

---
+++ app/Models/Chunk.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Pgvector\Laravel\HasNeighbors;
+
+class Chunk extends Model
+{   
+    use HasNeighbors;
+    protected $fillable=['document_id','chunk_index','metadata','embedding','content'];
+    protected $casts=['metadata'=>'array','embedding'=>Vector::class];
+
+      
+    public function document() : BelongsTo{
+        return $this->belongsTo(Document::class);
+    }
+}
---
+++ app/Models/Document.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Document extends Model
+{
+    protected $fillable=['title','content','mime_type','metadata'];
+    protected $casts=['metadata'=>'array'];
+    public function chunks(): HasMany{
+        return $this->hasMany(Chunk::class);
+    }
+}
